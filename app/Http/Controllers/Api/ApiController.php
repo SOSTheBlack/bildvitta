a5c4080fe3c0874783c1d2e5865806b7bb130a09
The Black MC (Jean C. Garcia) <jeancesargarcia@gmail.com>
2021-01-14 14:12:29 -0300

    creating controllers

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class Api
+ *
+ * @package App\Http\Controllers\APi
+ */
+class ApiController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/Api/Coins/ConversionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api\Coins;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+/**
+ * Class ConversionController
+ *
+ * @package App\Http\Controllers\APi\Coins
+ */
+class ConversionController  extends CoinController
+{
+    /**
+     * @param  Request  $request
+     *
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        dd('ddd');
+    }
+}
