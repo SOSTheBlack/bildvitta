2b2a1a3d98bfa4a23f81e1cf44b19e019261e1b8
The Black MC (Jean C. Garcia) <jeancesargarcia@gmail.com>
2021-01-14 14:11:04 -0300

    create middleware to check token

---
+++ app/Http/Middleware/CheckTokenApiMiddleware.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\Api\TokenException;
+use Closure;
+use Illuminate\Http\Request;
+
+/**
+ * Class PrivateApiMiddleware
+ *
+ * @package App\Http\Middleware
+ */
+class CheckTokenApiMiddleware
+{
+    /**
+     * Token key name.
+     *
+     * @const string
+     */
+    private const HEADER_TOKEN_KEY = 'token';
+
+    /**
+     * Token of private API.
+     *
+     * @var string
+     */
+    private string $privateToken;
+
+    /**
+     * @var Request
+     */
+    private Request $request;
+
+    /**
+     * PrivateApiMiddleware constructor.
+     */
+    public function __construct()
+    {
+        $this->privateToken = config('services.api.token');
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     *
+     * @return void
+     *
+     * @throws TokenException
+     */
+    public function handle(Request $request, Closure $next): void
+    {
+        $this->request = $request;
+
+        $this->hasTokenHeader();
+
+        $this->matchingTokenHeader();
+
+        $next($request);
+    }
+
+    /**
+     * @retunr void
+     *
+     * @throws TokenException
+     */
+    private function hasTokenHeader(): void
+    {
+        if (! $this->request->hasHeader(self::HEADER_TOKEN_KEY)) {
+            throw new TokenException('missing token, is has required');
+        }
+    }
+
+    /**
+     * @throws TokenException
+     *
+     * @retunr void
+     */
+    private function matchingTokenHeader(): void
+    {
+        if ($this->privateToken !== $this->request->header(self::HEADER_TOKEN_KEY)) {
+            throw new TokenException('invalid token');
+        }
+    }
+}
