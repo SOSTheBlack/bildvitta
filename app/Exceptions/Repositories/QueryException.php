15b8a95a8981d5cd6b91d81d15994841f85f737a
The Black MC (Jean C. Garcia) <jeancesargarcia@gmail.com>
2021-01-20 15:51:49 -0300

    finalizing documents

--- app/Exceptions/Repositories/QueryException.php
+++ app/Exceptions/Repositories/QueryException.php
@@ -5,6 +5,8 @@ namespace App\Exceptions\Repositories;
 /**
  * Class QueryException
  *
+ * 1001 - 'error when inserting database:'
+ *
  * @package Exceptions\Repositories
  */
 class QueryException extends RepositoryException
